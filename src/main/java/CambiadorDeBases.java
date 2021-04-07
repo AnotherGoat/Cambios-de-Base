d0c83b63e3c9bacfb0d80ef6d6c3a0565d95ea78
Víctor Mardones <v.mardones04@ufromail.cl>
2021-04-07 02:24:45 -0400

    Commit inicial

---
+++ src/main/java/CambiadorDeBases.java
@@ -0,0 +1,38 @@
+public class CambiadorDeBases {
+
+    public static void main(String[] args) {
+
+        int n = 1000;
+
+        System.out.println(aBinario(n));
+        System.out.println(aOctal(n));
+        System.out.println(aHexadecimal(n));
+    }
+
+    public static String base10aN(int numero, int baseN) {
+
+        int resto;
+        StringBuilder resultado = new StringBuilder();
+        String simbolos = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+        while (numero != 0) {
+            resto = numero % baseN;
+            resultado.insert(0, simbolos.charAt(resto));
+            numero = numero / baseN;
+        }
+
+        return resultado.toString();
+    }
+
+    public static String aBinario(int numero) {
+        return base10aN(numero, 2);
+    }
+
+    public static String aOctal(int numero) {
+        return base10aN(numero, 8);
+    }
+
+    public static String aHexadecimal(int numero) {
+        return base10aN(numero, 16);
+    }
+}
